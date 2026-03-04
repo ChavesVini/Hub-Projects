0a9c89436e8d1c1e821932a097014b336ab1884e
ChavesVini <vinidasilva2004@outlook.com>
2026-03-04 00:47:57 -0300

    :recycle: refactor avaliacao python

--- AVALIAÇÃO/avaliacao.py
+++ AVALIAÇÃO/avaliacao.py
@@ -1,26 +1,21 @@
 from lista_competencias import lista_competencias
 
-lista_letras = []
 soma = 0
 
-while len(lista_competencias) != len(lista_letras):
-  for competencias in lista_competencias:
-    nota = str(input(f'Qual sua nota para: {competencias}? '  
-          '(B - Bom, R - Regular e I - Ruim) ')).upper()
-    if all([nota != 'B', nota != 'R', nota != 'I']):
-      print(f'Letra inserida incorretamente! Tente novamente.')
-      lista_letras.clear()
-      break
-    else:
-      lista_letras.append(nota)
+for competencias in lista_competencias:
+  while True:
+    nota = input(f'Qual sua nota para: {competencias}? (B - Bom, R - Regular e I - Ruim) ').upper()
 
-for nota in lista_letras:
-  if nota == 'B':
-    soma += 3
-  elif nota == 'R':
-    soma += 2
-  elif nota == 'I':
-    soma += 1
+    if nota in ['B', 'R', 'I']:
+      if nota == 'B':
+        soma += 3
+      elif nota == 'R':
+        soma += 2
+      elif nota == 'I':
+        soma += 1
+      break 
 
-media = soma/5
-print(f'A sua média é: {media}')
+    else:    
+      print(f'Letra inserida incorretamente!')
+
+print("A sua média é: " + str(soma/len(lista_competencias)))
